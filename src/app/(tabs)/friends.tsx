1638fdd6e9cc5e3c4bca3094eb8f36730a2ef969
Abhisekh Pariyar <abhisekhpariyar88@gmail.com>
2026-04-02 22:33:01 +0545

    Add TabBottom

---
+++ src/app/(tabs)/friends.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function friends() {
+  return (
+    <View>
+      <Text>friends</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
